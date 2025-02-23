aae522a12dd3be2304c949ff821e7c54b97d515c
MetroidSkittles <brandon.mezei@me.com>
2025-02-22 22:06:39 -0500

    Header Fix

--- OrderTrak.Client/Layout/MainLayout.razor.cs
+++ OrderTrak.Client/Layout/MainLayout.razor.cs
@@ -4,6 +4,14 @@ namespace OrderTrak.Client.Layout
 {
     public partial class MainLayout
     {
-        public string HeaderMessage { get; set; } = "Welcome to OrderTrak";
+        protected string HeaderMessage { get; set; } = "Welcome to OrderTrak";
+        protected string SubTitle { get; set; } = "Your trusted order management system...";
+
+        public void UpdateHeader(string message, string subtitle)
+        {
+            HeaderMessage = message;
+            SubTitle = subtitle;
+            StateHasChanged();
+        }
     }
 }
--- OrderTrak.Client/Pages/Home.razor.cs
+++ OrderTrak.Client/Pages/Home.razor.cs
@@ -1,13 +1,17 @@
+using Microsoft.AspNetCore.Components;
+using OrderTrak.Client.Layout;
 using System.Runtime.CompilerServices;
 
 namespace OrderTrak.Client.Pages
 {
     public partial class Home
     {
+        [CascadingParameter]
+        public MainLayout MainLayout { get; set; } = default!;
 
         protected override void OnInitialized()
         {
-            MainLayout.HeaderMessage = "Welcome";
+            MainLayout.UpdateHeader("Welcome to OrderTrak.", "Test Update.");
         }
     }
 }
